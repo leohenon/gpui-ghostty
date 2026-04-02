2cb2ede2a5abbd992a2d380bcc6d79e67110380a
leohenon <77656081+lhenon999@users.noreply.github.com>
2026-04-02 09:56:40 +0800

    vt_dump: update for current ghostty_vt api

--- examples/vt_dump/src/main.rs
+++ examples/vt_dump/src/main.rs
@@ -4,10 +4,32 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     let mut input = Vec::new();
     std::io::stdin().read_to_end(&mut input)?;
 
-    let mut t = ghostty_vt::Terminal::new(80, 24)?;
-    t.feed(&input)?;
+    let terminal = ghostty_vt::Terminal::new(80, 24, 10_000)?;
+    terminal.vt_write(&input);
 
-    let s = t.dump_viewport()?;
-    print!("{s}");
+    let mut render = ghostty_vt::RenderState::new()?;
+    render.update(&terminal)?;
+
+    let mut buf = Vec::new();
+    let mut out = String::new();
+
+    render.begin_row_iteration();
+    while render.next_row() {
+        render.begin_cell_iteration();
+        while render.next_cell() {
+            let len = render.cell_graphemes(&mut buf);
+            if len == 0 {
+                out.push(' ');
+                continue;
+            }
+
+            for &cp in &buf[..len] {
+                out.push(char::from_u32(cp).unwrap_or(' '));
+            }
+        }
+        out.push('\n');
+    }
+
+    print!("{out}");
     Ok(())
 }
